09968bbba82e2bf32e366dab2afd5ade72923f34
andrew soulinhavong <soulinhavong@gmail.com>
2023-07-04 22:06:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'james'
+
+function upperCaseCustomerName(){
+    if(customerName === 'bob'){
+        customerName = customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newName){
+    bestCustomer = newName
+}
+
+function changeLeastFavoriteCustomer(){
+    throw new Error('Assignment to constant variable.');
+}
